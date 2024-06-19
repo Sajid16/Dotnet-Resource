c74d330ad708e88f236f9692ea7584bc00baa6de
Sajid16 <sajid.mahboob16@gmail.com>
2024-06-19 21:32:58 +0600

    multithread practiced within controllers

--- Cqrs_MeditrImplementation/Controllers/MultiThreadsController.cs
+++ Cqrs_MeditrImplementation/Controllers/MultiThreadsController.cs
@@ -31,48 +31,47 @@ namespace Cqrs_MediatR_Implementation.Controllers
                 students.Add(student);
             }
 
-            var skip = 0;
-            Thread[] tr = new Thread[4];
-            for (int j = 0; j < 4; j++)
-            {
-                List<StudentDetails> studentSubList = students.Skip(j * 100).Take(100).ToList();
-                //tr[t] = new Thread(new ThreadStart(MyThreadMethod(studentSubList)));
-                var threadIndex = j;
-
-                tr[j] = new Thread(() =>
-                {
-                    foreach (var item in studentSubList)
-                    {
-                        _logger.Information($"info is - {item.Id}:{item.StudentName}:{item.StudentAge} - Thread Name - {threadIndex}");
-                    }
-                });
-                tr[j].Start();
-
-            }
-
-            //// Define the number of threads you want to create
-            //int numberOfThreads = 4;
-
-            //// Create and start the threads using a loop
-            //for (int i = 0; i < numberOfThreads; i++)
+            //Thread[] tr = new Thread[4];
+            //for (int j = 0; j < 4; j++)
             //{
-            //    List<StudentDetails> studentSubList = students.Skip(i * 100).Take(100).ToList();
+            //    List<StudentDetails> studentSubList = students.Skip(j * 100).Take(100).ToList();
+            //    //tr[t] = new Thread(new ThreadStart(MyThreadMethod(studentSubList)));
+            //    var threadIndex = j;
 
-            //    // Capture the loop variable to avoid closure issues
-            //    int threadIndex = i;
-
-            //    // Create a new thread and assign it a task
-            //    Thread thread = new Thread(() =>
+            //    tr[j] = new Thread(() =>
             //    {
             //        foreach (var item in studentSubList)
             //        {
-            //            _logger.Information($"info is - {item.Id}:{item.StudentName}:{item.StudentAge} - Thread Index - {threadIndex}");
+            //            _logger.Information($"info is - {item.Id}:{item.StudentName}:{item.StudentAge} - Thread Name - {threadIndex}");
             //        }
             //    });
+            //    tr[j].Start();
 
-            //    // Start the thread
-            //    thread.Start();
             //}
+
+            // Define the number of threads you want to create
+            int numberOfThreads = 4;
+
+            // Create and start the threads using a loop
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                List<StudentDetails> studentSubList = students.Skip(i * 100).Take(100).ToList();
+
+                // Capture the loop variable to avoid closure issues
+                int threadIndex = i;
+
+                // Create a new thread and assign it a task
+                Thread thread = new Thread(() =>
+                {
+                    foreach (var item in studentSubList)
+                    {
+                        _logger.Information($"info is - {item.Id}:{item.StudentName}:{item.StudentAge} - Thread Index - {threadIndex}");
+                    }
+                });
+
+                // Start the thread
+                thread.Start();
+            }
 
             return Ok();
         }
